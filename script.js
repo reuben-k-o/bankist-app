89140cb59e90199d61ef92fe5e92b00f033bf9a5
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-03 22:00:34 +0300

    display movements with dates

--- script.js
+++ script.js
@@ -120,12 +120,19 @@ const displayMovements = function (acc, sort = false) {
     : acc.movements;
   movs.forEach((mov, i) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formattedCur(mov, acc.locale, acc.currency);
+
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${Math.abs(mov.toFixed(2))}€</div>
+    <div class="movements__date">${displayDate}</div> 
+    <div class="movements__value">${formattedMov}</div>
   </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
